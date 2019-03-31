c09da5c16073104bf76ac61c41e13568debc0587
tiffany1618 <tiffany9342@gmail.com>
2019-03-31 10:50:47 -0700

    Small fixes

--- frontend/src/js/components/TimelineItem.js
+++ frontend/src/js/components/TimelineItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { API_ENDPOINT_TOPICS, API_ENDPOINT } from './../constants/CONFIG';
 import { TimelineItem }  from 'vertical-timeline-component-for-react';
 import Topic from './Topic.js';
+import { formatDate } from '../util/formatDate';
 
 const axios = require('axios');
 
@@ -37,27 +38,11 @@ class TimeLineItem extends React.Component {
             console.log(error);
         });
 	}
-
-	formatDate(date) {
-		date = 	String(date).slice(0, String(date).length-1).split('T');
-		let days = String(date[0]).split('-');
-		let mins = String(date[1]).split(':');
-
-		let time;
-		if (mins[0] == 12)
-			time = `12:${mins[1]}pm`;
-		else if (mins[0] == 0)
-			time = `12:${mins[1]}am`;
-		else
-			time = mins[0] < 12 ? `${mins[0]}:${mins[1]}am` : `${mins[0] - 12}:${mins[1]}pm`;
-
-		return (`${days[1]}/${days[2]}/${days[0]}, ${time}`);
-	}
 	
 	render() {
 		return (
 			<TimelineItem 
-				dateText={this.formatDate(this.state.date)}
+				dateText={formatDate(this.state.date)}
 				dateInnerStyle={{ background: '#2FD3F8' }}
 				style={{ color: '#2FD3F8' }}
 				bodyContainerStyle={{
@@ -67,7 +52,7 @@ class TimeLineItem extends React.Component {
 			    }}
 			>
 				{this.state.topics.map((topic) => (
-					<Topic date={this.formatDate(this.state.date)} topic={topic} />
+					<Topic date={this.state.date} topic={topic} />
 				))}
 			</TimelineItem>
 		);
